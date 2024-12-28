641749d00a41da7c49ba9e0cafbd54a543228fdf
Antoniomary <amanso.totti@yahoo.com>
2024-12-28 21:10:24 +0000

    created auth.js to authenticate requests to server

---
+++ api/middlewares/v1/auth.js
@@ -0,0 +1,33 @@
+import { ObjectId } from 'mongodb';
+import dbClient from '../../utils/db.js';
+import redisClient from '../../utils/redis.js';
+
+const auth = async (req, res, next) => {
+  let token = null;
+
+  const reqJson = req.headers.accept === 'application/json';
+
+  if (reqJson) token = req.headers['x-token'];
+  else token = req.cookies['quotilate_token'];
+
+  if (!token) {
+    if (reqJson) return res.status(401).json({ error: 'Unauthorized' });
+    return res.redirect('/login');
+  }
+
+  if (!dbClient.isAlive() || !redisClient.isAlive()) {
+    return res.status(500).json({ error: 'unable to process request' });
+  }
+
+  const id = await redisClient.get(`auth_${token}`);
+  if (!id) return res.status(401).json({ error: 'Unauthorized' });
+
+  const user = await dbClient.db.collection('users').findOne({ _id: new ObjectId(id) });
+  if (!user) return res.status(401).json({ error: 'Unauthorized' });
+
+  req.user = user;
+
+  next();
+}
+
+export default auth;
