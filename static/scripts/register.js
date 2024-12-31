c2ab54e5829b0e251261be6e737b37a036315eeb
Antoniomary <amanso.totti@yahoo.com>
2024-12-31 16:34:47 +0000

    handled the error aspect

--- static/scripts/register.js
+++ static/scripts/register.js
@@ -1,59 +1,46 @@
+import { changeType, showFlashMessage } from './utils.js';
+
 document.addEventListener('DOMContentLoaded', () => {
-    const passShowHide = document.getElementById('pass-show-hide');
-    passShowHide.addEventListener('click', () => changeType('pass'));
+  const passShowHide = document.getElementById('pass-show-hide');
+  passShowHide.addEventListener('click', () => changeType('pass'));
 
-    const rePassShowHide = document.getElementById('re-pass-show-hide');
-    rePassShowHide.addEventListener('click', () => changeType('re-pass'));
+  const rePassShowHide = document.getElementById('re-pass-show-hide');
+  rePassShowHide.addEventListener('click', () => changeType('re-pass'));
 
-    const registerForm = document.getElementById('register-form');
-    registerForm.addEventListener('submit', async (e) => {
-        e.preventDefault();
+  const registerForm = document.getElementById('register-form');
+  registerForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
-        const username = document.getElementById('user').querySelector('input').value;
-        const email = document.getElementById('email').querySelector('input').value;
-        const password = document.getElementById('pass').querySelector('input').value;
-        const repassword = document.getElementById('re-pass').querySelector('input').value;
+    const username = document.getElementById('user').querySelector('input').value;
+    const email = document.getElementById('email').querySelector('input').value;
+    const password = document.getElementById('pass').querySelector('input').value;
+    const repassword = document.getElementById('re-pass').querySelector('input').value;
 
-        try {
-            const res = await fetch('/register',
-                {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        username,
-                        email,
-                        password,
-                        repassword,
-                    }),
-                }
-            );
-
-            if (!res.ok) {
-                alert('i was here');
-                const result = await res.json();
-                return showFlashMessage(result.error, true);
-            }
-
-            window.location.href = '/dashboard';
-        } catch(err) {
-            showFlashMessage('Sorry, try again later.', true);
+    try {
+      const res = await fetch('/register',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            username,
+            email,
+            password,
+            repassword,
+          }),
         }
-    });
-});
+      );
 
-function changeType(loginInput) {
-    const inputField = document.getElementById(loginInput).querySelector('input');
-    const passShowHide = document.getElementById(loginInput).querySelector('div');
+      if (!res.ok) {
+        alert('i was here');
+        const result = await res.json();
+        return showFlashMessage(result.error, true);
+      }
 
-    if (inputField.type === 'text') {
-        inputField.type = 'password';
-        passShowHide.style.backgroundImage = "url('../images/bx-hide.svg')";
-    } else {
-        inputField.type = 'text';
-        passShowHide.style.backgroundImage = "url('../images/bx-show.svg')";
+      window.location.href = '/dashboard';
+    } catch(err) {
+      showFlashMessage('Sorry, try again later.', true);
     }
-
-    inputField.focus();
-}
+  });
+});
