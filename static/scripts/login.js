f5fd4fad9d3f2f4f5cecf424895dc7d6ddec9863
Antoniomary <amanso.totti@yahoo.com>
2024-12-31 15:32:35 +0000

    handled the error aspect

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -1,51 +1,39 @@
+import { changeType, showFlashMessage } from './utils.js';
+
 document.addEventListener('DOMContentLoaded', () => {
-    const passShowHide = document.getElementById('pass-show-hide');
-    passShowHide.addEventListener('click', () => changeType('pass'));
+  const passShowHide = document.getElementById('pass-show-hide');
+  passShowHide.addEventListener('click', () => changeType('pass'));
 
-    const loginForm = document.getElementById('login-form');
-    loginForm.addEventListener('submit', async (e) => {
-        e.preventDefault();
+  const loginForm = document.getElementById('login-form');
+  loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
-        const identifier = document.getElementById('user').querySelector('input').value;
-        const password = document.getElementById('pass').querySelector('input').value;
+    const identifier = document.getElementById('user').querySelector('input').value;
+    const password = document.getElementById('pass').querySelector('input').value;
 
-        try {
-            const res = await fetch('/login',
-                {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        username: identifier,
-                        email: identifier,
-                        password,
-                    }),
-                }
-            );
-            
-            if (res.ok) {
-                window.location.href = '/dashboard';
-            } else {
-                alert('sorry');
-            }
-        } catch (err) {
-            // console.log(err.message);
+    try {
+      const res = await fetch('/login',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            username: identifier,
+            email: identifier,
+            password,
+          }),
         }
-    });
-});
+      );
+            
+      if (!res.ok) {
+        const result = await res.json();
+        return showFlashMessage(result.error, true);
+      }
 
-function changeType(loginInput) {
-    const inputField = document.getElementById(loginInput).querySelector('input');
-    const passShowHide = document.getElementById(loginInput).querySelector('div');
-
-    if (inputField.type === 'text') {
-        inputField.type = 'password';
-        passShowHide.style.backgroundImage = "url('../images/bx-hide.svg')";
-    } else {
-        inputField.type = 'text';
-        passShowHide.style.backgroundImage = "url('../images/bx-show.svg')";
+      window.location.href = '/dashboard';
+    } catch (err) {
+      showFlashMessage('Sorry, try again later', true);
     }
-
-    inputField.focus();
-}
+  });
+});
