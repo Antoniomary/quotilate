2f0ce60213fa0215223a5b54a116db9a25bba43c
Antoniomary <amanso.totti@yahoo.com>
2024-12-31 16:14:00 +0000

    reduced tabshift width to 2 spaces

--- static/scripts/utils.js
+++ static/scripts/utils.js
@@ -1,29 +1,29 @@
 export function changeType(loginInput) {
-    const inputField = document.getElementById(loginInput).querySelector('input');
-    const passShowHide = document.getElementById(loginInput).querySelector('div');
+  const inputField = document.getElementById(loginInput).querySelector('input');
+  const passShowHide = document.getElementById(loginInput).querySelector('div');
 
-    if (inputField.type === 'text') {
-        inputField.type = 'password';
-        passShowHide.style.backgroundImage = "url('../images/bx-hide.svg')";
-    } else {
-        inputField.type = 'text';
-        passShowHide.style.backgroundImage = "url('../images/bx-show.svg')";
-    }
+  if (inputField.type === 'text') {
+    inputField.type = 'password';
+    passShowHide.style.backgroundImage = "url('../images/bx-hide.svg')";
+  } else {
+    inputField.type = 'text';
+    passShowHide.style.backgroundImage = "url('../images/bx-show.svg')";
+  }
 
-    inputField.focus();
+  inputField.focus();
 }
 
 export function showFlashMessage(message, isError=false) {
-    const flashMessage = document.getElementById('flash-message');
-    
-    flashMessage.innerText = message;
-    flashMessage.style.backgroundColor = isError ? '#f44336' : '#4caf50';
+  const flashMessage = document.getElementById('flash-message');
 
-    flashMessage.classList.remove('hidden');
-    flashMessage.classList.add('visible');
+  flashMessage.innerText = message;
+  flashMessage.style.backgroundColor = isError ? '#f44336' : '#4caf50';
 
-    setTimeout(() => {
-        flashMessage.classList.remove('visible');
-        flashMessage.classList.add('hidden');       
-    }, 3000);
+  flashMessage.classList.remove('hidden');
+  flashMessage.classList.add('visible');
+
+  setTimeout(() => {
+    flashMessage.classList.remove('visible');
+    flashMessage.classList.add('hidden');
+  }, 3000);
 }
