9ed95ea2cbe676d457525aa3a2916a3d336ac38a
Antoniomary <amanso.totti@yahoo.com>
2024-12-31 16:41:22 +0000

    moved some functions to utils

--- static/scripts/quote_actions.js
+++ static/scripts/quote_actions.js
@@ -1,135 +1,106 @@
+import { showFlashMessage } from './utils.js';
+
 document.addEventListener('DOMContentLoaded', () => {
-    (async () => {
-        try {
-            await generateQuote();
-        } catch(err) {
-            showFlashMessage('Unable to load quote! Pls, Refresh page.', true);
-        }
-    })();
+  (async () => {
+    try {
+      await generateQuote();
+    } catch(err) {
+      showFlashMessage('Unable to load quote! Pls, Refresh page.', true);
+    }
+  })();
 
-    const getNewQuote = document.getElementById('get-quote');
-    getNewQuote.addEventListener('click', async () => {
-        try {
-            await generateQuote();
-        } catch(err) {
-            showFlashMessage(err.message, true);
-        }
-    });
+  const getNewQuote = document.getElementById('get-quote');
+  getNewQuote.addEventListener('click', async () => {
+    try {
+      await generateQuote();
+    } catch(err) {
+      showFlashMessage(err.message, true);
+    }
+  });
 
-    const copyBtn = document.getElementById('copy');
-    copyBtn.addEventListener('click', copyText);
+  const copyBtn = document.getElementById('copy');
+  copyBtn.addEventListener('click', copyText);
 
-    const shareBtn = document.getElementById('share');
-    shareBtn.addEventListener('click', () => {
-        const sharingPlatform = document.getElementsByClassName('sharing-platform-container')[0];
+  const shareBtn = document.getElementById('share');
+  shareBtn.addEventListener('click', () => {
+    const sharingPlatform = document.getElementsByClassName('sharing-platform-container')[0];
 
-        sharingPlatform.classList.remove('hidden');
-        sharingPlatform.classList.add('visible');
+    sharingPlatform.classList.remove('hidden');
+    sharingPlatform.classList.add('visible');
 
-        setTimeout(() => {
-            sharingPlatform.classList.remove('visible');
-            sharingPlatform.classList.add('hidden');       
-        }, 12000);
-    });
+    setTimeout(() => {
+      sharingPlatform.classList.remove('visible');
+      sharingPlatform.classList.add('hidden');       
+    }, 12000);
+  });
 
-    const twitterShare = document.getElementById('twitter-share');
-    twitterShare.addEventListener('click', shareOnTwitter);
+  const twitterShare = document.getElementById('twitter-share');
+  twitterShare.addEventListener('click', shareOnTwitter);
 
-    const facebookShare = document.getElementById('facebook-share');
-    facebookShare.addEventListener('click', shareOnFacebook);
+  const facebookShare = document.getElementById('facebook-share');
+  facebookShare.addEventListener('click', shareOnFacebook);
 
-    const saveBtn = document.getElementById('save');
-    saveBtn.addEventListener('click', saveQuote);
+  const saveBtn = document.getElementById('save');
+  saveBtn.addEventListener('click', saveQuote);
 });
 
 async function generateQuote() {
-    try {
-        // const response = await fetch('http://localhost:5000/quote');
-            
-        let response = [
-            {
-                quote: 'The mountain you see today you shall see no more',
-                author: '2AI',
-            },
-            {
-                quote: 'Suffer a witch not to live',
-                author: 'Anthony',
-            },
-            {
-                quote: 'We only live once, but if we do it right, once is enough',
-                author: 'Mai West',
-            },
-        ];
+  try {
+    const response = await fetch('/quote');
 
-        const choice = Math.floor(Math.random() * 3);
-        response = response[choice];
+    if (!response.ok) return showFlashMessage('Sorry, try again later.', true);
 
-        const quote = document.getElementById('quote');
-        const quoteAuthor = document.getElementById('quote-author');
+    const quote = document.getElementById('quote');
+    const quoteAuthor = document.getElementById('quote-author');
 
-        quote.classList.add('hidden', 'fade-out');
-        quoteAuthor.classList.add('hidden', 'fade-out');
-        setTimeout(() => {
-            quote.innerText = response.quote;    
-            quoteAuthor.innerText = response.author;
-            quote.classList.remove('hidden');
-            quoteAuthor.classList.remove('hidden');
-        }, 1000);
-    } catch(err) {
-        throw Error('Unable to generate quote! Pls, try again.');
-    }
+    quote.classList.add('hidden', 'fade-out');
+    quoteAuthor.classList.add('hidden', 'fade-out');
+    setTimeout(() => {
+      quote.innerText = response.quote;    
+      quoteAuthor.innerText = response.author;
+      quote.classList.remove('hidden');
+      quoteAuthor.classList.remove('hidden');
+    }, 1000);
+  } catch(err) {
+    showFlashMessage('Unable to generate quote! Pls, try again.', true);
+  }
 }
 
 function copyText() {
-    const text = getQuoteAndAuthor();
+  const text = getQuoteAndAuthor();
 
-    navigator.clipboard.writeText(text)
-        .then(() => {
-            showFlashMessage('Quote copied to clipboard!');
-        })
-        .catch((err) => {
-            showFlashMessage('Failed to copy quote', true);
-        })
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      showFlashMessage('Quote copied to clipboard!');
+    })
+    .catch((err) => {
+      showFlashMessage('Failed to copy quote', true);
+    })
 }
 
 function shareOnTwitter() {
-    const textToShare = getQuoteAndAuthor();
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(textToShare)}`;
+  const textToShare = getQuoteAndAuthor();
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(textToShare)}`;
 
-    window.open(twitterUrl, '_blank');
+  window.open(twitterUrl, '_blank');
 }
 
 function shareOnFacebook() {
-    const textToShare = getQuoteAndAuthor();
-    const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}&quote=${encodeURIComponent(textToShare)}`;
+  const textToShare = getQuoteAndAuthor();
+  const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}&quote=${encodeURIComponent(textToShare)}`;
 
-    window.open(facebookUrl, '_blank');
+  window.open(facebookUrl, '_blank');
 }
 
 async function saveQuote() {
-    //check if user logged in.
-    // if yes, save else return the login page
-    showFlashMessage('Quote has been saved!');
+  //check if user logged in.
+  // if yes, save else return the login page
+  showFlashMessage('Quote has been saved!');
 }
 
 function getQuoteAndAuthor() {
-    const quote = document.getElementById('quote').innerText;
-    const quoteAuthor = document.getElementById('quote-author').innerText;
+  const quote = document.getElementById('quote').innerText;
+  const quoteAuthor = document.getElementById('quote-author').innerText;
     
-    return `"${quote}" -- ${quoteAuthor}`;
-}
-
-function showFlashMessage(message, isError=false) {
-    const flashMessage = document.getElementById('flash-message');
-    
-    flashMessage.innerText = message;
-    flashMessage.style.backgroundColor = isError ? '#f44336' : '#4caf50';
-
-    flashMessage.classList.remove('hidden');
-    flashMessage.classList.add('visible');
-
-    setTimeout(() => {
-        flashMessage.classList.remove('visible');
-        flashMessage.classList.add('hidden');       
-    }, 3000);
+  return `"${quote}" -- ${quoteAuthor}`;
 }
