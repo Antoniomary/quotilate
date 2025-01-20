d47acfe50c8878418cff95ee6d07845b0b57baa9
Antoniomary <amanso.totti@yahoo.com>
2025-01-20 23:02:28 +0000

    refactored and documented code

--- api/utils/db.js
+++ api/utils/db.js
@@ -2,23 +2,29 @@ import { MongoClient } from 'mongodb';
 
 class DBClient {
   constructor() {
+    // Get connection parameters from environment variables or fallback to defaults
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const dbName = process.env.DB_DATABASE || 'quotilate';
 
+    // Use these parameters to construct the MongoDB connection URL
     const url = `mongodb://${host}:${port}`;
-    this.client = new MongoClient(url, { useUnifiedTopology: true });
-    this.connected = false;
 
-    this.client.connect()
-      .then(() => {
-        this.db = this.client.db(dbName);
-        this.connected = true;
-        console.log(`Connected to ${dbName} database`);
-      })
-      .catch((err) => {
-        console.log(`Failed to connect to mongodb: ${err}`);
-      });
+    this.client = new MongoClient(url);
+    this.dbName = dbName;
+    this.connected = false;
+  }
+
+  async connect() {
+    try {
+      await this.client.connect()
+      this.db = this.client.db(this.dbName);
+      this.connected = true;
+      console.log(`Connected to ${this.dbName} database`);
+    } catch(err) {
+      console.log(`Failed to connect to MongoDB: ${err}`);
+      process.exit(1); // Exit process if database connection fails
+    }
   }
 
   isAlive() {
@@ -35,5 +41,13 @@ class DBClient {
 }
 
 const db = new DBClient();
+
+(async () => {
+  try {
+    await db.connect(); // Connect to the database
+  } catch(err) {
+    console.error(err);
+  }
+})();
 
 export default db;
