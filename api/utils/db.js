b88b6dc0d2d60fbe79a82e6970ebb0ec88222916
Antoniomary <amanso.totti@yahoo.com>
2025-01-20 22:15:59 +0000

    added more documentation

--- api/utils/db.js
+++ api/utils/db.js
@@ -15,39 +15,48 @@ class DBClient {
     this.connected = false;
   }
 
+  // Async method to connect to the database
   async connect() {
     try {
+      // Try to establish the connection to MongoDB
       await this.client.connect()
-      this.db = this.client.db(this.dbName);
-      this.connected = true;
-      console.log(`Connected to ${this.dbName} database`);
+      this.db = this.client.db(this.dbName); // Set the database object
+      this.connected = true; // Mark the connection as successful
+      console.log(`Connected to ${this.dbName} database`); // Log success message
     } catch(err) {
+      // If there's an error, log it and exit the process
       console.log(`Failed to connect to MongoDB: ${err}`);
-      process.exit(1); // Exit process if database connection fails
+      process.exit(1);
     }
   }
 
+  // Check if the database connection is alive
   isAlive() {
     return this.connected;
   }
 
+  // Get the number of users in the 'users' collection
   async nbUsers() {
     return this.db.collection('users').countDocuments();
   }
 
+  // Get the number of quotes in the 'quotes' collection
   async nbQuotes() {
     return this.db.collection('quotes').countDocuments();
   }
 }
 
+// Create an instance of DBClient
 const db = new DBClient();
 
+// Immediately invoke the connect method to establish a connection
 (async () => {
   try {
     await db.connect(); // Connect to the database
   } catch(err) {
-    console.error(err);
+    console.error(err); // Handle errors if the connection fails
   }
 })();
 
+// Export the DBClient instance for use in other parts of the application
 export default db;
