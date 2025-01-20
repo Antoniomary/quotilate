6f501dbfff3a21a2143356a3dfe01b411217b4a0
Antoniomary <amanso.totti@yahoo.com>
2025-01-20 22:58:08 +0000

    created index.js

---
+++ api/controllers/v1/index.js
@@ -0,0 +1,6 @@
+import AppController from './AppController.js';
+import ViewsController from './ViewsController.js';
+import UsersController from './UsersController.js';
+import QuotesController from './QuotesController.js';
+
+export { AppController, ViewsController, UsersController, QuotesController };
