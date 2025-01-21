64679d830468f67428ab86b79eee06f37e2c7ce5
Antoniomary <amanso.totti@yahoo.com>
2025-01-21 00:02:19 +0000

    added documentation

--- api/controllers/v1/AppController.js
+++ api/controllers/v1/AppController.js
@@ -2,6 +2,13 @@ import cache from '../../utils/redis.js';
 import db from '../../utils/db.js';
 
 class AppController {
+  /**
+   * Returns the status of the cache and database.
+   *
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   * @returns {Object} - Status of cache and db connections.
+   */
   static getStatus(req, res) {
     return res.status(200).json({
       cache: cache.isAlive(),
@@ -9,6 +16,13 @@ class AppController {
     });
   }
 
+  /**
+   * Returns the statistics for users and quotes.
+   *
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   * @returns {Object} - Number of users and quotes in the database.
+   */
   static async getStats(req, res) {
     return res.status(200).json({
       users: await db.nbUsers(),
