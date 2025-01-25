6b315773f940dbc344a4b449a310008729ef5190
Antoniomary <amanso.totti@yahoo.com>
2025-01-25 22:59:29 +0000

    added documentations

--- static/scripts/utils.js
+++ static/scripts/utils.js
@@ -1,7 +1,14 @@
+/**
+ * Toggles the visibility of a password input field and changes the associated
+ * icon to show or hide the password.
+ * 
+ * @param {string} loginInput - The ID of the password input field container.
+ */
 export function changeType(loginInput) {
   const inputField = document.getElementById(loginInput).querySelector('input');
   const passShowHide = document.getElementById(loginInput).querySelector('div');
 
+  // Toggle the input type between 'text' and 'password'
   if (inputField.type === 'text') {
     inputField.type = 'password';
     passShowHide.style.backgroundImage = "url('../images/bx-hide.svg')";
@@ -10,18 +17,29 @@ export function changeType(loginInput) {
     passShowHide.style.backgroundImage = "url('../images/bx-show.svg')";
   }
 
+  // Focus the input field after toggling
   inputField.focus();
 }
 
+/**
+ * Displays a flash message to the user. The message will disappear after a set time.
+ * 
+ * @param {string} message - The message to be displayed.
+ * @param {boolean} [isError=false] - Indicates whether the message is an error. Defaults to false.
+ */
 export function showFlashMessage(message, isError=false) {
   const flashMessage = document.getElementById('flash-message');
 
+  // Set the flash message text
   flashMessage.innerText = message;
+  // Change background color based on error status
   flashMessage.style.backgroundColor = isError ? '#f44336' : '#4caf50';
 
+  // Show the flash message
   flashMessage.classList.remove('hidden');
   flashMessage.classList.add('visible');
 
+  // Hide the flash message after 3 seconds
   setTimeout(() => {
     flashMessage.classList.remove('visible');
     flashMessage.classList.add('hidden');
