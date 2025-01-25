6b5c5dcf109b27d5dedd999af791fa6df880da3d
Antoniomary <amanso.totti@yahoo.com>
2025-01-25 23:18:00 +0000

    added documentations

--- api/server.js
+++ api/server.js
@@ -6,29 +6,42 @@ import cors from 'cors';
 import routes from './routes/v1/index.js';
 import logger from './middlewares/v1/logger.js';
 
+// Define default host and port
 const PORT = process.env.PORT || 1245;
 const HOST = process.env.HOST || '127.0.0.1';
 
+// Initialize the Express app
 const app = express();
 
+// Get the directory of the current module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Middleware to handle CORS
 app.use(cors({
-  origin: ['*'],
-  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
-  credentials: true,
+  origin: ['*'], // Allow all origins
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+  credentials: true, // Include credentials in requests
 }));
 
+// Set view engine to EJS for rendering templates
 app.set('views', path.join(__dirname, '../static/views'));
 app.set('view engine', 'ejs');
 
+// Middleware for parsing JSON and cookies
 app.use(express.json());
 app.use(cookieParser());
+
+// Serve static files from the "static" directory
 app.use(express.static(path.join(__dirname, '../static')));
+
+// Custom middleware for logging
 app.use(logger);
+
+// Route definitions
 app.use(routes);
 
+// Function to populate the database on server startup
 (async () => {
   try {
     const response = await fetch(`http://${HOST}:${PORT}/quote`);
@@ -39,6 +52,7 @@ app.use(routes);
   }
 })();
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running at ${HOST} on ${PORT}`);
 });
