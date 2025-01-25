c45671f1968764112056722921e9ffdff56d7396
Antoniomary <amanso.totti@yahoo.com>
2025-01-25 22:50:40 +0000

    added documentations

--- static/scripts/dashboard.js
+++ static/scripts/dashboard.js
@@ -1,5 +1,8 @@
 import { showFlashMessage } from './utils.js';
 
+/**
+ * Event listener for the DOMContentLoaded event to initialize the page and its actions.
+ */
 document.addEventListener('DOMContentLoaded', () => {
   const quotesContainer = document.getElementById('quotes-container');
   const myQuotes = quotesContainer.getElementsByClassName('my-quote');
@@ -9,9 +12,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const action = queryParams.get('action');
   const quoteId = queryParams.get('quoteId');
 
+  // Update the URL to remove query parameters
   const newUrl = window.location.origin + window.location.pathname;
   window.history.replaceState({}, '', newUrl);
 
+  /**
+   * Saves a quote by sending a POST request to the server.
+   * Displays a success or error message based on the server response.
+   */
   if (action === 'save' && quoteId) {
     const saveQuote = async () => {
       try {
@@ -22,6 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
           data.savedAt = new Date(data.savedAt).toGMTString();
 
+          // Create a new quote element and append it to the container
           const newQuoteElement = document.createElement('div');
           newQuoteElement.className = 'my-quote';
           newQuoteElement.setAttribute('data-id', data.id);
@@ -34,6 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
             <p class="user-date">${data.savedAt}</p>
           `;
 
+          // Update the total number of saved quotes
           const totalNumberOfQuotes = document.getElementById('number-of-quotes');
           const currentCount = parseInt(totalNumberOfQuotes.innerText.match(/\d+/)[0], 10) || 0;
           totalNumberOfQuotes.innerText = `Total saved Quotes: ${currentCount + 1}`;
@@ -57,12 +67,16 @@ document.addEventListener('DOMContentLoaded', () => {
     saveQuote();
   }
 
+   // Logout functionality
   const logoutBtn = document.getElementById('logout');
   logoutBtn.addEventListener('click', async () => {
     await fetch('/logout', { method: 'POST' });
     window.location.href = '/login';
   });
 
+  /**
+   * Toggle the display of saved quotes dropdown on button click.
+   */
   const dropDownUp = document.getElementsByClassName('drop-down-up')[0];
   dropDownUp.addEventListener('click', () => {
       const saved = document.getElementById('saved');
@@ -82,8 +96,12 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  // Search functionality
   const searchInput = document.getElementById('search-input');
 
+  /**
+   * Changes the search input field based on selected filter category.
+   */
   const filterCategory = document.getElementById('filter');
   filterCategory.addEventListener('change', () => {
     if (filterCategory.value === 'date') {
@@ -103,6 +121,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  /**
+   * Filters the quotes based on the input value and selected filter category.
+   */
   document.getElementById('search-input').addEventListener('input', () => {
     if (['quote', 'author'].includes(filterCategory.value)) {
       const input = searchInput.value.toLowerCase();
@@ -143,6 +164,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   let selected;
 
+  /**
+   * Handles the click event for viewing a selected quote.
+   */
   quotesContainer.addEventListener('click', (event) => {
     const myQuote = event.target.closest('.my-quote');
 
@@ -167,6 +191,9 @@ document.addEventListener('DOMContentLoaded', () => {
     hideOverlay('.overlay#view-quote-overlay');
   });
 
+  /**
+   * Copies the selected quote and author to clipboard.
+   */
   const savedCopy = document.getElementById('saved-copy');
   savedCopy.addEventListener('click', copySaved);
 
@@ -183,12 +210,21 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 4000);
   });
 
+  /**
+   * Handles sharing the quote on Twitter.
+   */
   const savedTwitterShare = document.getElementById('saved-twitter-share');
   savedTwitterShare.addEventListener('click', shareOnTwitter);
 
+  /**
+   * Handles sharing the quote on Facebook.
+   */
   const savedFacebookShare = document.getElementById('saved-facebook-share');
   savedFacebookShare.addEventListener('click', shareOnFacebook);
 
+  /**
+   * Displays the confirmation overlay for quote deletion.
+   */
   const deleteQuote = document.getElementById('saved-delete');
   deleteQuote.addEventListener('click', () => showOverlay('.overlay#confirm-delete'));
 
@@ -230,6 +266,9 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+/**
+ * Copies the quote and author to the clipboard.
+ */
 function copySaved() {
   const text = getQuoteAndAuthor();
 
@@ -242,6 +281,9 @@ function copySaved() {
     })
 }
 
+/**
+ * Shares the quote on Twitter.
+ */
 function shareOnTwitter() {
   const textToShare = getQuoteAndAuthor();
   const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(textToShare)}`;
@@ -249,6 +291,9 @@ function shareOnTwitter() {
   window.open(twitterUrl, '_blank');
 }
 
+/**
+ * Shares the quote on Facebook.
+ */
 function shareOnFacebook() {
   const textToShare = getQuoteAndAuthor();
   const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}&quote=${encodeURIComponent(textToShare)}`;
@@ -256,14 +301,26 @@ function shareOnFacebook() {
   window.open(facebookUrl, '_blank');
 }
 
+/**
+ * Hides the overlay element.
+ * @param {string} element The CSS selector of the overlay element to hide.
+ */
 function hideOverlay(element) {
   document.querySelector(element).style.visibility = 'hidden';
 }
 
+/**
+ * Shows the overlay element.
+ * @param {string} element The CSS selector of the overlay element to show.
+ */
 function showOverlay(element) {
   document.querySelector(element).style.visibility = 'visible';
 }
 
+/**
+ * Retrieves the quote and author text from the overlay.
+ * @returns {string} The quote and author formatted as a string.
+ */
 function getQuoteAndAuthor() {
   const q = document.getElementById('q').innerText;
   const a = document.getElementById('a').innerText;
@@ -271,6 +328,11 @@ function getQuoteAndAuthor() {
   return `${q} ${a}`;
 }
 
+/**
+ * Converts a date to the format DD-MM-YYYY.
+ * @param {string} format The date to be formatted.
+ * @returns {string} The formatted date.
+ */
 function toDDMMYYYY(format) {
   const date = new Date(format);
 
